74212b858fa02d8e00aa1efde683416892c17177
Frank Xu <frank.xu@northbright.com>
2020-11-22 13:12:03 +0800

    Add LoadParticipants() and LoadConfig()

--- lottery.go
+++ lottery.go
@@ -1,8 +1,17 @@
 package lottery
 
 import (
-//"encoding/json"
-//"fmt"
+	//"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	//"math/rand"
+	"path"
+	//"sync"
+	//"time"
+
+	"github.com/northbright/csvhelper"
+	"github.com/northbright/pathhelper"
 )
 
 type Participant struct {
@@ -27,7 +36,39 @@ type Config struct {
 }
 
 type Lottery struct {
-	Config
-	Participants []Participant
-	Winners      map[int][]Participant
+	config       Config
+	participants []Participant
+	winners      map[int][]Participant
+}
+
+func (l *Lottery) LoadParticipants(file string) error {
+	currentDir, _ := pathhelper.GetCurrentExecDir()
+	file = path.Join(currentDir, file)
+
+	rows, err := csvhelper.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	l.participants = []Participant{}
+	for _, row := range rows {
+		if len(row) != 2 {
+			return fmt.Errorf("incorrect participants CSV")
+		}
+		l.participants = append(l.participants, Participant{row[0], row[1]})
+	}
+	return nil
+}
+
+func (l *Lottery) LoadConfig(file string) error {
+	currentDir, _ := pathhelper.GetCurrentExecDir()
+	file = path.Join(currentDir, file)
+
+	// Load Conifg.
+	buf, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(buf, &l.config)
 }
